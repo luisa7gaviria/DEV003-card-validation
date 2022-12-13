594231bb2f881f68471d9fb0a579bf68afb4537d
Luisa <lorenitaa889@gmail.com>
2022-12-13 15:29:46 -0500

    cambios temporales

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import validator from './validator.js';
 
 console.log(validator);
+const formulario= document.getElementById('btn');
+
+
+
+formulario.addEventListener("click", function(e){
+  const tarjeta = document.getElementById("card");
+  
+  e.preventDefault();
+  data();
+  alert(validator.maskify(tarjeta.value));
+  //console.log(validator.maskify(tarjeta.value));
+  
+
+});
+
+function data(){
+  const nombp=document.getElementById("name").value;
+  const cardp=document.getElementById("card").value;
+
+  if(nombp===""){
+    alert("El nombre es obligatorio");
+    document.getElementById("name").focus();
+  }
+  else{
+    if(cardp===""){
+      alert("El número de tarjeta es obligatorio");
+      document.getElementById("card").focus();
+      
+    }
+  }
+  
+  console.log(nombp + cardp);
+}
+
+
+
+ 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,34 @@
 const validator = {
-  // ...
+  maskify: function(text) {
+  
+    const cardNum= text.length;
+    let mask ="";
+    if (text.length > 4){
+      for(let i=0; cardNum -4; i++){
+        mask = mask + "#"
+      }
+      const lastFour=text.slice(-4);
+      const masked= mask + lastFour;
+      return masked;
+    } else{
+      return cardNum;
+    }
+
+  },
+ 
+    
+  
+  validate: function isValid(){
+    
+    
+    
+    
+    
+
+  }
 };
 
 export default validator;
+  
+  
+
